47564c6b481f3e95731285ea0944e6b8baa3865f
neknalb <malte.blanken@me.com>
2025-04-11 16:29:10 +0200

    BonsaiWriter theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
 	import Section_2 from '$lib/components/Section_2.svelte'
 </script>
 
+<!--
+<h1>Eine Überschrift</h1>
+<p>Ein gewöhnlicher Absatz.</p>
+<p>Und noch ein gewöhnlicher Absatz.</p>
+-->
+
 <section class="mb-12">
 	<Section_1 />
 </section>
@@ -12,6 +18,6 @@
 </section>
 
 <section class="mb-12">
-	<h2>Plant your idea</h2>
+	<h2 class="mb-4 text-2xl">Plant your idea</h2>
 	<p>Let your ideas become text. Without obstacles.</p>
 </section>
