1bc4bc4c8b607f0435309153aa8ec102b403f2ec
neknalb <malte.blanken@me.com>
2025-04-24 22:35:38 +0200

    Improve header and footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,5 @@
 <svelte:options runes={true} />
 
-<footer class="bg-primary-300 p-4">(footer)</footer>
+<footer class="mt-4 border-t-1 p-4 text-xs">
+	Contact: <a class="anchor" href="https://gitlab.com/neknalb">https://gitlab.com/neknalb</a>
+</footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,15 +1,24 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
+	import { goto } from '$app/navigation'
 	import { page } from '$app/state'
 	import logo from '$lib/images/logo.png'
 	let route = $derived(page.route)
 </script>
 
-<header class="relative h-20 border-b-1 bg-white px-8 py-4">
-	<img src={logo} alt="Bonsai Writer logo" class="absolute left-10 inline-block h-12" />
-	<div class="absolute top-7 right-10 flex space-x-4">
-		<a href="/" class={route.id === '/' ? 'text-primary-500' : 'text-gray-700'}>Home</a>
-		<a href="/whats-new" class={route.id === '/whats-new' ? 'text-primary-500' : 'text-gray-700'}>What's new</a>
-	</div>
+<header class="flex h-20 items-center justify-between px-8 py-8">
+	<img src={logo} alt="Bonsai Writer logo" class="h-12" />
+	<nav class="flex space-x-4">
+		<button
+			class={['btn hover:preset-tonal', route.id === '/' ? 'underline' : '']}
+			type="button"
+			onclick={() => goto('/')}>Home</button
+		>
+		<button
+			class={['btn hover:preset-tonal', route.id === '/whats-new' ? 'underline' : '']}
+			type="button"
+			onclick={() => goto('/whats-new')}>What's new</button
+		>
+	</nav>
 </header>
