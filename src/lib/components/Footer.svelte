fd435e64b9eabc82412906c8b7903c7cdfa9df59
neknalb <malte.blanken@me.com>
2025-05-16 21:04:49 +0200

    Update contact info

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <svelte:options runes={true} />
 
 <footer class="mt-4 border-t-1 p-4 text-xs">
-	Contact: <a class="anchor" href="https://gitlab.com/neknalb">https://gitlab.com/neknalb</a>
+	Contact: bonsai-writer-team <i>(at)</i> online.de
 </footer>
