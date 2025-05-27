058accd9146a518b42e1a1d16dc9a8499341959e
neknalb <malte.blanken@me.com>
2025-05-27 13:21:48 +0200

    Add GitHub reference to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,8 @@
 <svelte:options runes={true} />
 
 <footer class="mt-4 border-t-1 p-4 text-xs">
-	Contact: bonsai-writer-team <i>(at)</i> online.de
+	Source Code <a class="anchor" href="https://github.com/neknalb/bonsai-writer-website-rebuild"
+		>at GitHub</a
+	>
+	| Contact: bonsai-writer-team <i>(at)</i> online.de
 </footer>
