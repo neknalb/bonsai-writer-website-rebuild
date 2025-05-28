501fa31e3360356f5e76e9157f35954f03e76e2a
neknalb <malte.blanken@me.com>
2025-05-28 22:07:11 +0200

    UI improvements

--- src/lib/components/News.svelte
+++ src/lib/components/News.svelte
@@ -7,13 +7,16 @@
 <div class="news my-4">
 	<h3>{title}</h3>
 	<small>{date}</small>
-	<SvelteMarkdown class="news" source={content} />
+	<div class="ml-8">
+		<SvelteMarkdown class="news" source={content} />
+	</div>
 </div>
 
 <style>
 	.news {
+		color: rgb(128, 128, 128);
 		:global(p) {
-			margin: 10px;
+			margin-bottom: 6px;
 		}
 		:global(small) {
 			font-weight: 100;
@@ -21,12 +24,11 @@
 		:global(h3) {
 			font-size: 34px;
 			font-weight: 900;
-			color: rgb(100, 100, 100);
+			color: rgb(90, 90, 90);
 		}
 		:global(h4) {
 			font-size: 22px;
 			font-weight: 900;
-			color: rgb(128, 128, 128);
 			margin-top: 24px;
 			margin-bottom: 8px;
 		}
@@ -37,11 +39,11 @@
 			margin-bottom: 4px;
 		}
 		:global(ul) {
-			list-style-type: circle;
+			list-style-type: disc;
 			list-style-position: inside;
 		}
 		:global(li) {
-			margin: 10px 0;
+			margin: 12px 0;
 		}
 	}
 </style>
